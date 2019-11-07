a4c42b7942283a8b3cd805766732b25fe48f07e3
kdcao <kdcao@cmc.com.vn>
2019-11-07 15:50:38 +0700

    add parameter year

--- sample/Main.java
+++ sample/Main.java
@@ -10,20 +10,35 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
 
+        int year = 2019;
+
         for (int i = 1; i <= 53; i++) {
-            main.getStartAndEndDate(i);
+            main.getStartAndEndDate(i, year);
         }
     }
 
-    private void getStartAndEndDate(int week) {
+    private void getStartAndEndDate(int week, int year) {
         System.out.println("Week: " + week);
         Calendar cal = Calendar.getInstance();
+
+        // set Year
+        cal.set(Calendar.YEAR, year);
+
+        // set week of year
         cal.set(Calendar.WEEK_OF_YEAR, week);
+
+        // format date to view
         DateFormat df = new SimpleDateFormat("EEE yyyy-MM-dd");
+
+        // set day start of week is sunday
         cal.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
         System.out.println(df.format(cal.getTime()));
+
+        // add 6 days to become end-day is saturday
         cal.add(Calendar.DATE, 6);
+
         System.out.println(df.format(cal.getTime()));
+        
         System.out.println("============");
     }
 }
